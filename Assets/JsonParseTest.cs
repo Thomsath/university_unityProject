4d3209b0f408407df8cea4738cc8b2d3b0053e22
User-VR <User-VR@INFO-VEGETA.iut.bx1>
2018-02-09 11:19:39 +0100

    Editing Update jsonParse.cs

--- Assets/JsonParseTest.cs
+++ Assets/JsonParseTest.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using LitJson;
-using UnityEditor;
 using UnityEngine.UI;
 using System;
 
@@ -18,16 +17,11 @@ public class JsonParseTest : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
-        //Debug.Log(itemData["Weapons"][0]["name"]);
-        //Debug.Log(itemData["body"]["measuregrps"][0]["grpid"]);
         string url = "http://api.health.nokia.com/measure?action=getmeas&oauth_consumer_key=6901bc0863d8b2b110757d6edf48ff64676b4137f4e05574f431f7431175&oauth_nonce=8db1e10c1a06fb4cff09e75c5fb83089&oauth_signature=IHWedoJk5pSuCN1QoX0%2BQbAbbnk%3D&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1517838908&oauth_token=8ef03624c0916d27d6fd6fbd1395cb6fbf86718c0e73645198cd9f31de3e22&oauth_version=1.0&userid=15399388";
         WWW www = new WWW(url);
-        StartCoroutine(getJSONData(www));
-
         // On ecrit dans le fichier les nouvelles données
-
-
+        StartCoroutine(getJSONData(www));
+ 
         //On récupère le JSON par le fichier
         JSONString = File.ReadAllText(Application.dataPath + "/data.json");
         Debug.Log(JSONString);
@@ -40,20 +34,22 @@ public class JsonParseTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        string url = "http://api.health.nokia.com/measure?action=getmeas&oauth_consumer_key=6901bc0863d8b2b110757d6edf48ff64676b4137f4e05574f431f7431175&oauth_nonce=8db1e10c1a06fb4cff09e75c5fb83089&oauth_signature=IHWedoJk5pSuCN1QoX0%2BQbAbbnk%3D&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1517838908&oauth_token=8ef03624c0916d27d6fd6fbd1395cb6fbf86718c0e73645198cd9f31de3e22&oauth_version=1.0&userid=15399388";
+        WWW www = new WWW(url);
+        StartCoroutine(getJSONData(www));
+        //On récupère le JSON par le fichier
+        JSONString = File.ReadAllText(Application.dataPath + "/data.json");
+        
+        //Convertir JSON par un objet
+        itemData = JsonMapper.ToObject(JSONString);
 
+        m_MyText.text = GetItemandWritewithType();
     }
 
     JsonData GetItem(int item, string type)
     {
 
         return itemData["body"]["measuregrps"][item][type];
-
-        /*for (int i = 0; i < itemData[type].Count; i++)
-        {
-            if((itemData["body"]["measuregrps"][i][name] == )
-        }
-        return null;*/
-
     }
 
     string GetItemandWritewithType()
@@ -83,22 +79,6 @@ public class JsonParseTest : MonoBehaviour
 
                 switch (type)
                 {
-                    case "1":
-                        //Poids
-                        text += "Poids : " + val.Substring(0, 2) + "." + val.Substring(2, 2) + " kg\n";
-                        break;
-                    case "4":
-                        //Taille
-                        text += "Taille : " + val.Substring(0, 1) + "." + val.Substring(1, 2) + " m\n";
-                        break;
-                    case "9":
-                        //Diastolic
-                        text += "Diastolic : " + val + "\n";
-                        break;
-                    case "10":
-                        //Systolic
-                        text += "Sytolic : " + val + "\n";
-                        break;
                     case "11":
                         //BPM (Battements par minute)
                         text += "BPM : " + val + "\n";
@@ -108,10 +88,6 @@ public class JsonParseTest : MonoBehaviour
                         string dizaine = val.Substring(0, 2);
                         string unite = val.Substring(2, 1);
                         text += "Temperature : " + Math.Round(Convert.ToDouble(dizaine + "." + unite), 1) + "\n";
-                        break;
-
-                    default:
-                        text += "Pas encore fait" + "\n";
                         break;
                 }
 
